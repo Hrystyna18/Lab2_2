88241b5fc05fc0f26eab3e331486659b77a39020
Hrystyna18 <tymkivhrystyna007@icloud.com>
2020-04-22 23:33:22 +0300

    Add project files.

---
+++ Lab2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP_lab2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (double x = 0; x <  Math.PI; x += Math.PI / 20)
+            {
+                double y = Math.Tan(x) ;
+                Console.WriteLine("x = {0:f4} y = {1:f4}", x, y);
+            }
+        }
+    }
+}
